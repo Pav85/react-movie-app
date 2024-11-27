e772aa1903849e131af3e89de08f020957674e35
Pawel <pawel.werbowy@gmail.com>
2024-11-27 22:33:00 +0000

    pages created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,7 @@
 import "./App.css";
-import MovieCard from "./components/MovieCard";
 
 const App = () => {
-  return (
-    <>
-      <MovieCard movie={{ title: "Matrix", release_date: "1999" }} />
-    </>
-  );
+  return <></>;
 };
 
 export default App;

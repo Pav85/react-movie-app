0f7c1d2750ec54657efb25a4a502045a59af8e48
Pawel <pawel.werbowy@gmail.com>
2024-11-27 22:09:20 +0000

    Update MovieCard.jsx

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,5 +1,21 @@
-const MovieCard = () => {
-  return <div>MovieCard</div>;
+const MovieCard = ({ movie }) => {
+  const handleFavouriteClick = () => {
+    console.log("Favourite button clicked");
+  };
+
+  return (
+    <div className="movie-card">
+      <div className="movie-poster">
+        <img src={movie.poster} alt={movie.title} />
+        <div className="movie-overlay">
+          <button
+            className="favourite-btn"
+            onClick={handleFavouriteClick}
+          ></button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MovieCard;

143a8958df9ecb0ebe7b34e107cc1c469259ce59
Pawel <pawel.werbowy@gmail.com>
2024-11-29 11:55:39 +0000

    navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+
+import Favorites from "./pages/Favorites";
 import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <>
-      <Home />
-    </>
+    <div>
+      <Navbar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </div>
   );
 };
 

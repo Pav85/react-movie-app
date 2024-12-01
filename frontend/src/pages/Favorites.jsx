cc09acaa0289f11b1d1275eab66413736a82807e
Pawel <pawel.werbowy@gmail.com>
2024-12-01 23:46:24 +0000

    Update Favorites.jsx

--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -4,6 +4,19 @@ import MovieCard from "../components/MovieCard";
 
 const Favorite = () => {
   const { favorites } = useMovieContext();
+
+  if (favorites) {
+    return (
+      <div className="favorites">
+        <h2>Favorite Movies</h2>
+        <div className="movies-grid">
+          {favorites.map((movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="favorites-empty">
       <h2>No favorite movies yet</h2>

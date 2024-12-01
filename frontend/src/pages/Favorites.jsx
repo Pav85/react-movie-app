f47de1c361a12a2ba0757ccdc82acb0c6f7d8af3
Pawel <pawel.werbowy@gmail.com>
2024-12-01 23:38:50 +0000

    Update Favorites.jsx

--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -1,6 +1,9 @@
 import "../css/Favorites.css";
+import { useMovieContext } from "../contexts/useMovieContext";
+import MovieCard from "../components/MovieCard";
 
 const Favorite = () => {
+  const { favorites } = useMovieContext();
   return (
     <div className="favorites-empty">
       <h2>No favorite movies yet</h2>

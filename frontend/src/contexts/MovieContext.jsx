1e813afa143ddb25baeaf332b0f39c30f700ebf3
Pawel <pawel.werbowy@gmail.com>
2024-12-01 23:11:59 +0000

    Update MovieContext.jsx

--- frontend/src/contexts/MovieContext.jsx
+++ frontend/src/contexts/MovieContext.jsx
@@ -2,4 +2,22 @@ import { createContext, useContext, useState, useEffect } from "react";
 
 const MovieContext = createContext();
 
-export const useMovieContext = () => {};
+export const useMovieContext = () => useContext(MovieContext);
+
+export const MovieProvider = ({ children }) => {
+  const [favorites, setFavorites] = useState([]);
+  4;
+
+  useEffect(() => {
+    const storedFavorites = localStorage.getItem("favorites");
+    if (storedFavorites) {
+      setFavorites(JSON.parse(storedFavorites));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  }, [favorites]);
+
+  return <MovieContext.Provider>{children}</MovieContext.Provider>;
+};
